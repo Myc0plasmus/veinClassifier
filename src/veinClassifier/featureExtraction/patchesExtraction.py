fdb9507950aae753f55e6864969084230821b7ce
Myc0plasmus <jablonski.natan@gmail.com>
2026-06-13 09:03:54 +0000

    fixed import

--- src/veinClassifier/featureExtraction/patchesExtraction.py
+++ src/veinClassifier/featureExtraction/patchesExtraction.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from tqdm import tqdm
-from src.dataset import EyeDataset
+from veinClassifier.dataset import EyeDataset
 
 def extractImagePatches(img, kernel_size=5):
     img = img.unsqueeze(0)  
